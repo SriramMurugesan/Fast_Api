f0a011261ea6ab64210d3a2c12015e57011fa9f1
SriramMurugesan <sri2014ram@gmail.com>
2025-02-23 14:20:50 +0530

    1.30hrs

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel, field_validator
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -9,3 +20,9 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data":"this is yout posts"}
+
+@app.post("/createposts")
+def create_posts(post:Post):
+    print(post.rating)
+    print(post.dict())
+    return {"data":post} 
